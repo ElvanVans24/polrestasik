1ef96d379fb9bc4a5e00e12c75d452c5068081cc
ElvanVans24 <elvanvans24@gmail.com>
2025-04-19 16:04:57 +0700

    Add files via upload

--- pages/berita/index.vue
+++ pages/berita/index.vue
@@ -15,7 +15,8 @@
       <div class="card shadow p-3 mb-5 bg-body-tertiary mx-1 rounded">
         <img src="/img/berita2.jpg" class="card-img-top" alt="Berita 2">
         <div class="card-body d-flex flex-column justify-content-between">
-          <h6 class="card-title">Polres Tasikmalaya Kota Gelar Apel Kesiapan Siagaan dan Apel Premanisme Bersama Forkopimda</h6>
+          <h6 class="card-title">Polres Tasikmalaya Kota Gelar Apel Kesiapan Siagaan dan Apel Premanisme Bersama
+            Forkopimda</h6>
           <div class="">
             <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
             <a href="/berita/zona" class="btn bg-warning bg-gradient">Selengkapnya</a>
@@ -25,7 +26,8 @@
       <div class="card shadow p-3 mb-5 bg-body-tertiary mx-1 rounded">
         <img src="/img/berita3.jpg" class="card-img-top" alt="Berita 3">
         <div class="card-body d-flex flex-column justify-content-between">
-          <h6 class="card-title">Polres Tasikmalaya Kota Gelar Apel Gabungan Kesiapan Siagaan untuk Operasi Ketupat Lodaya2025</h6>
+          <h6 class="card-title">Polres Tasikmalaya Kota Gelar Apel Gabungan Kesiapan Siagaan untuk Operasi Ketupat
+            Lodaya2025</h6>
           <div class="">
             <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
             <a href="/berita/sc2024" class="btn bg-warning bg-gradient">Selengkapnya</a>
@@ -35,7 +37,8 @@
       <div class="card shadow p-3 mb-5 bg-body-tertiary mx-1 rounded">
         <img src="/img/berita4.jpg" class="card-img-top" alt="Berita 4">
         <div class="card-body d-flex flex-column justify-content-between">
-          <h6 class="card-title">Kapolres Tasikmalaya Kota menerapkan rekayasa lalu lintas di Simpang Tiga Pamoyanan</h6>
+          <h6 class="card-title">Kapolres Tasikmalaya Kota menerapkan rekayasa lalu lintas di Simpang Tiga Pamoyanan
+          </h6>
           <div class="">
             <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
             <a href="/berita/us" class="btn bg-warning bg-gradient">Selengkapnya</a>
@@ -45,7 +48,8 @@
       <div class="card shadow p-3 mb-5 bg-body-tertiary mx-1 rounded">
         <img src="/img/berita5.jpg" class="card-img-top" alt="Berita 5">
         <div class="card-body d-flex flex-column justify-content-between">
-          <h6 class="card-title">Dalam rangka memastikan keamanan dan kelancaran arus mudik Lebaran, Kapolres Tasikmalaya Kota melakukan monitoring di Ampera Waterpark</h6>
+          <h6 class="card-title">Dalam rangka memastikan keamanan dan kelancaran arus mudik Lebaran, Kapolres
+            Tasikmalaya Kota melakukan monitoring di Ampera Waterpark</h6>
           <div class="">
             <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
             <a href="/berita/upacara" class="btn bg-warning bg-gradient">Selengkapnya</a>
@@ -53,6 +57,120 @@
         </div>
       </div>
     </div>
+
+    <div class="card-group pt-3 pb-3 berita_cards">
+      <div class="card shadow p-3 mb-5 bg-body-tertiary mx-1 rounded">
+        <img src="/img/berita6.jpg" class="card-img-top" alt="Berita 1">
+        <div class="card-body d-flex flex-column justify-content-between">
+          <h6 class="card-title">Dalam rangka memastikan keamanan dan kenyamanan wisatawan selama arus mudik Lebaran,
+            Kapolres Tasikmalaya Kota melakukan monitoring di Objek Wisata Galunggung.</h6>
+          <div class="">
+            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            <a href="/berita/berita6" class="btn bg-warning bg-gradient">Selengkapnya</a>
+          </div>
+        </div>
+      </div>
+      <div class="card shadow p-3 mb-5 bg-body-tertiary mx-1 rounded">
+        <img src="/img/berita7.jpg" class="card-img-top" alt="Berita 2">
+        <div class="card-body d-flex flex-column justify-content-between">
+          <h6 class="card-title">Pengawalan Ambulans oleh Polres Tasikmalaya Kota untuk Keamanan Pasien</h6>
+          <div class="">
+            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            <a href="/berita/berita7" class="btn bg-warning bg-gradient">Selengkapnya</a>
+          </div>
+        </div>
+      </div>
+      <div class="card shadow p-3 mb-5 bg-body-tertiary mx-1 rounded">
+        <img src="/img/berita8.jpg" class="card-img-top" alt="Berita 3">
+        <div class="card-body d-flex flex-column justify-content-between">
+          <h6 class="card-title">Kapolres Tasikmalaya Kota Evakuasi Penumpang Bis Mogok di Tanjakan Gentong untuk Cegah
+            Kemacetan Arus Balik</h6>
+          <div class="">
+            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            <a href="/berita/berita8" class="btn bg-warning bg-gradient">Selengkapnya</a>
+          </div>
+        </div>
+      </div>
+      <div class="card shadow p-3 mb-5 bg-body-tertiary mx-1 rounded">
+        <img src="/img/berita9.jpg" class="card-img-top" alt="Berita 4">
+        <div class="card-body d-flex flex-column justify-content-between">
+          <h6 class="card-title">Kapolres Tasikmalaya Kota Bersama Brimob Evakuasi Bus Mogok di Tikungan Gentong</h6>
+          <div class="">
+            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            <a href="/berita/berita9" class="btn bg-warning bg-gradient">Selengkapnya</a>
+          </div>
+        </div>
+      </div>
+      <div class="card shadow p-3 mb-5 bg-body-tertiary mx-1 rounded">
+        <img src="/img/berita10.jpg" class="card-img-top" alt="Berita 5">
+        <div class="card-body d-flex flex-column justify-content-between">
+          <h6 class="card-title">Polres Tasikmalaya Kota Terapkan One Way untuk Lancarkan Arus Balik Lebaran 2025</h6>
+          <div class="">
+            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            <a href="/berita/berita10" class="btn bg-warning bg-gradient">Selengkapnya</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="card-group pt-3 pb-3 berita_cards">
+      <div class="card shadow p-3 mb-5 bg-body-tertiary mx-1 rounded">
+        <img src="/img/berita11.jpg" class="card-img-top" alt="Berita 1">
+        <div class="card-body d-flex flex-column justify-content-between">
+          <h6 class="card-title">Kapolres Tasikmalaya Kota Imbau Waspada di Tanjakan Gentong, Arus Lalu Lintas H+7
+            Lancar</h6>
+          <div class="">
+            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            <a href="/berita/berita11" class="btn bg-warning bg-gradient">Selengkapnya</a>
+          </div>
+        </div>
+      </div>
+      <div class="card shadow p-3 mb-5 bg-body-tertiary mx-1 rounded">
+        <img src="/img/berita12.jpg" class="card-img-top" alt="Berita 2">
+        <div class="card-body d-flex flex-column justify-content-between">
+          <h6 class="card-title">Polwan Tasikmalaya Berikan Sentuhan Humanis kepada Pemudik di Arus Balik Lebaran</h6>
+          <div class="">
+            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            <a href="/berita/berita12" class="btn bg-warning bg-gradient">Selengkapnya</a>
+          </div>
+        </div>
+      </div>
+      <div class="card shadow p-3 mb-5 bg-body-tertiary mx-1 rounded">
+        <img src="/img/berita13.jpg" class="card-img-top" alt="Berita 3">
+        <div class="card-body d-flex flex-column justify-content-between">
+          <h6 class="card-title">Bhabinkamtibmas Polsek Cihideung Naik Pangkat, Aiptu Dadang Riawan Terima Penghargaan
+            Kenaikan Pangkat Pengabdian</h6>
+          <div class="">
+            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            <a href="/berita/berita13" class="btn bg-warning bg-gradient">Selengkapnya</a>
+          </div>
+        </div>
+      </div>
+      <div class="card shadow p-3 mb-5 bg-body-tertiary mx-1 rounded">
+        <img src="/img/berita14.jpg" class="card-img-top" alt="Berita 4">
+        <div class="card-body d-flex flex-column justify-content-between">
+          <h6 class="card-title">Kapolres Tasikmalaya Kota Hadiri Deklarasi Pilkada Damai Jelang PSU Kabupaten
+            Tasikmalaya: Semua Calon Berkomitmen Wujudkan PSU Aman dan Damai
+          </h6>
+          <div class="">
+            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            <a href="/berita/berita14" class="btn bg-warning bg-gradient">Selengkapnya</a>
+          </div>
+        </div>
+      </div>
+      <div class="card shadow p-3 mb-5 bg-body-tertiary mx-1 rounded">
+        <img src="/img/berita15.jpg" class="card-img-top" alt="Berita 5">
+        <div class="card-body d-flex flex-column justify-content-between">
+          <h6 class="card-title">Kapolres Tasikmalaya Kota Launching Program “Polwan Mengajar di Madrasah” untuk Perkuat
+            Pendidikan Karakter Pelajar</h6>
+          <div class="">
+            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            <a href="/berita/berita15" class="btn bg-warning bg-gradient">Selengkapnya</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <!-- Add more card groups as needed, truncated for brevity -->
   </div>
 </template>
--- pages/galery/index.vue
+++ pages/galery/index.vue
@@ -1,7 +1,25 @@
 <template>
   <div class="container my-5">
-    <h1 class="text-center mb-5 prestasi_title">PRESTASI POLRES TASIKMALAYA KOTA</h1>
+    <h1 class="text-center mb-5 prestasi_title">GALERY POLRES TASIKMALAYA KOTA</h1>
     <div class="row row-cols-1 row-cols-md-3 g-4 prestasi_cards">
+      <div class="col">
+        <div class="card">
+          <img src="/img/srl.jpg" class="card-img-top" alt="Safety Riding">
+          <div class="card-body">
+            <h5 class="card-title">Pengujian Lintasan Safety Riding SMKN4 Tasikmalaya</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="/img/srl.jpg" class="card-img-top" alt="Safety Riding">
+          <div class="card-body">
+            <h5 class="card-title">Pengujian Lintasan Safety Riding SMKN4 Tasikmalaya</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+      </div>
       <div class="col">
         <div class="card">
           <img src="/img/srl.jpg" class="card-img-top" alt="Safety Riding">
