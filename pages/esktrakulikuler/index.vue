e00ac4c8f91a15e113a48a06f40780c565d23d4d
ElvanVans24 <elvanvans24@gmail.com>
2025-04-21 16:05:33 +0700

    Add files via upload

--- pages/esktrakulikuler/index.vue
+++ pages/esktrakulikuler/index.vue
@@ -2,15 +2,15 @@
   <div class="container-fluid pt-3 pb-3 bg-secondary">
     <h1 class="text-center text-white informasi_title">INFORMASI</h1>
     <div class="d-flex justify-content-center">
-      <div class="row row-cols-1 row-cols-md-2 g-4 justify-content-center informasi_cards">
+      <div class="row row-cols-1 row-cols-sm-2 g-3 justify-content-center informasi_cards">
         <div class="col d-flex justify-content-center">
-          <div class="card mb-4" style="max-width: 400px;">
+          <div class="card mb-3" style="max-width: 400px; width: 100%;">
             <img src="/img/layanan1.jpg" class="card-img-top" alt="Layanan 1">
             <div class="card-body"></div>
           </div>
         </div>
         <div class="col d-flex justify-content-center">
-          <div class="card mb-4" style="max-width: 400px;">
+          <div class="card mb-3" style="max-width: 400px; width: 100%;">
             <img src="/img/layanan2.jpg" class="card-img-top" alt="Layanan 2">
             <div class="card-body"></div>
           </div>
@@ -18,8 +18,8 @@
       </div>
     </div>
 
-    <div class="d-flex justify-content-center mt-5">
-      <div class="card text-center shadow-lg aduan_card" style="width: 22rem;">
+    <div class="d-flex justify-content-center mt-4">
+      <div class="card text-center shadow-lg aduan_card" style="max-width: 22rem; width: 100%;">
         <div class="card-body">
           <h5 class="card-title fw-bold">Aduan Masyarakat</h5>
           <p class="card-text">Laporkan kejadian atau aduan langsung ke Polres Tasikmalaya Kota melalui link berikut:</p>
@@ -28,17 +28,17 @@
       </div>
     </div>
 
-    <div class="row py-5">
+    <div class="row py-4">
       <div class="col">
         <h1 class="text-center text-white layanan_title">LAYANAN</h1>
-        <div class="row row-cols-1 row-cols-md-3 g-4 layanan_cards">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 layanan_cards">
           <div class="col">
-            <div class="card mb-4" style="max-width: 540px;">
+            <div class="card mb-3" style="max-width: 540px; width: 100%;">
               <div class="row g-0">
-                <div class="col-md-4">
+                <div class="col-4 col-md-4">
                   <img src="/img/SPDT.jpg" class="img-fluid rounded-start" alt="DUMAS PRESISI">
                 </div>
-                <div class="col-md-8">
+                <div class="col-8 col-md-8">
                   <div class="card-body">
                     <h5 class="card-title">DUMAS PRESISI</h5>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
@@ -50,12 +50,12 @@
             </div>
           </div>
           <div class="col">
-            <div class="card mb-4" style="max-width: 540px;">
+            <div class="card mb-3" style="max-width: 540px; width: 100%;">
               <div class="row g-0">
-                <div class="col-md-4">
+                <div class="col-4 col-md-4">
                   <img src="/img/SATINTELKAM.jpg" class="img-fluid rounded-start" alt="SATINTELKAM">
                 </div>
-                <div class="col-md-8">
+                <div class="col-8 col-md-8">
                   <div class="card-body">
                     <h5 class="card-title">INFORMASI SATINTELKAM</h5>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
@@ -67,12 +67,12 @@
             </div>
           </div>
           <div class="col">
-            <div class="card mb-4" style="max-width: 540px;">
+            <div class="card mb-3" style="max-width: 540px; width: 100%;">
               <div class="row g-0">
-                <div class="col-md-4">
+                <div class="col-4 col-md-4">
                   <img src="/img/SKCK.jpg" class="img-fluid rounded-start" alt="SKCK">
                 </div>
-                <div class="col-md-8">
+                <div class="col-8 col-md-8">
                   <div class="card-body">
                     <h5 class="card-title">INFORMASI SKCK</h5>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
@@ -84,12 +84,12 @@
             </div>
           </div>
           <div class="col">
-            <div class="card mb-4" style="max-width: 540px;">
+            <div class="card mb-3" style="max-width: 540px; width: 100%;">
               <div class="row g-0">
-                <div class="col-md-4">
+                <div class="col-4 col-md-4">
                   <img src="/img/SIM.jpg" class="img-fluid rounded-start" alt="SIM">
                 </div>
-                <div class="col-md-8">
+                <div class="col-8 col-md-8">
                   <div class="card-body">
                     <h5 class="card-title">INFORMASI SIM</h5>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
@@ -101,12 +101,12 @@
             </div>
           </div>
           <div class="col">
-            <div class="card mb-4" style="max-width: 540px;">
+            <div class="card mb-3" style="max-width: 540px; width: 100%;">
               <div class="row g-0">
-                <div class="col-md-4">
+                <div class="col-4 col-md-4">
                   <img src="/img/SPKT.jpg" class="img-fluid rounded-start" alt="SPKT">
                 </div>
-                <div class="col-md-8">
+                <div class="col-8 col-md-8">
                   <div class="card-body">
                     <h5 class="card-title">INFORMASI SPKT</h5>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
@@ -118,12 +118,12 @@
             </div>
           </div>
           <div class="col">
-            <div class="card mb-4" style="max-width: 540px;">
-              <div class="row g-0 img">
-                <div class="col-md-4 w-full">
+            <div class="card mb-3" style="max-width: 540px; width: 100%;">
+              <div class="row g-0">
+                <div class="col-4 col-md-4">
                   <img src="/img/layanan1.jpg" class="img-fluid rounded-start" alt="Testimoni">
                 </div>
-                <div class="col-md-8">
+                <div class="col-8 col-md-8">
                   <div class="card-body">
                     <h5 class="card-title">TESTIMONI</h5>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
@@ -148,60 +148,97 @@ onMounted(() => {
   // Animate informasi title
   gsap.from('.informasi_title', {
     opacity: 0,
-    y: 100,
-    duration: 1,
+    y: 50,
+    duration: 0.8,
     ease: 'power2.out'
   })
 
   // Animate informasi cards
   gsap.from('.informasi_cards .card', {
     opacity: 0,
-    y: 50,
-    duration: 0.8,
+    y: 30,
+    duration: 0.6,
     ease: 'power3.out',
     stagger: 0.1,
-    delay: 0.5
+    delay: 0.3
   })
 
   // Animate aduan card
   gsap.from('.aduan_card', {
     opacity: 0,
-    scale: 0.8,
-    duration: 1,
+    scale: 0.9,
+    duration: 0.8,
     ease: 'power3.out',
-    delay: 0.7
+    delay: 0.5
   })
 
   // Animate layanan title
   gsap.from('.layanan_title', {
     opacity: 0,
-    y: 100,
-    duration: 1,
+    y: 50,
+    duration: 0.8,
     ease: 'power2.out',
-    delay: 0.9
+    delay: 0.7
   })
 
   // Animate layanan cards
   gsap.from('.layanan_cards .card', {
     opacity: 0,
-    y: 50,
-    duration: 0.8,
+    y: 30,
+    duration: 0.6,
     ease: 'power3.out',
     stagger: 0.1,
-    delay: 1.1
+    delay: 0.9
   })
 })
 </script>
 
 <style scoped>
 .card-img-top {
-  height: 200px;
+  height: 180px;
   object-fit: cover;
 }
 
 .img-fluid {
   height: 100%;
-  object-fit:contain;
-  margin-left: 4%;
-} 
+  object-fit: cover;
+}
+
+@media (max-width: 576px) {
+  .card-img-top {
+    height: 150px;
+  }
+
+  .informasi_title, .layanan_title {
+    font-size: 1.5rem;
+  }
+
+  .card-body {
+    padding: 0.75rem;
+  }
+
+  .card-title {
+    font-size: 1rem;
+  }
+
+  .card-text {
+    font-size: 0.875rem;
+  }
+
+  .btn {
+    font-size: 0.875rem;
+    padding: 0.5rem 1rem;
+  }
+
+  .informasi_cards .card,
+  .layanan_cards .card,
+  .aduan_card {
+    max-width: 100% !important;
+  }
+
+  .row-cols-sm-2 > .col {
+    flex: 0 0 100%;
+    max-width: 100%;
+  }
+}
 </style>
