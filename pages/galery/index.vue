58fb766dd785d0a872afaaff388a9b66a105d3c0
ElvanVans24 <elvanvans24@gmail.com>
2025-04-23 12:33:44 +0700

    Add files via upload

--- pages/galery/index.vue
+++ pages/galery/index.vue
@@ -4,7 +4,7 @@
     <div class="row row-cols-1 row-cols-md-3 g-4 prestasi_cards">
       <div class="col">
         <div class="card">
-          <img src="/img/srl.jpg" class="card-img-top" alt="Safety Riding">
+          <img src="/img/galery1.jpeg" class="card-img-top" alt="Safety Riding">
           <div class="card-body">
             <h5 class="card-title">Pengujian Lintasan Safety Riding SMKN4 Tasikmalaya</h5>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
@@ -13,7 +13,7 @@
       </div>
       <div class="col">
         <div class="card">
-          <img src="/img/srl.jpg" class="card-img-top" alt="Safety Riding">
+          <img src="/img/galery2.jpeg" class="card-img-top" alt="Safety Riding">
           <div class="card-body">
             <h5 class="card-title">Pengujian Lintasan Safety Riding SMKN4 Tasikmalaya</h5>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
@@ -22,7 +22,7 @@
       </div>
       <div class="col">
         <div class="card">
-          <img src="/img/srl.jpg" class="card-img-top" alt="Safety Riding">
+          <img src="/img/galery3.jpeg" class="card-img-top" alt="Safety Riding">
           <div class="card-body">
             <h5 class="card-title">Pengujian Lintasan Safety Riding SMKN4 Tasikmalaya</h5>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
@@ -31,7 +31,7 @@
       </div>
       <div class="col">
         <div class="card">
-          <img src="/img/srl.jpg" class="card-img-top" alt="Safety Riding">
+          <img src="/img/galery4.jpeg" class="card-img-top" alt="Safety Riding">
           <div class="card-body">
             <h5 class="card-title">Pengujian Lintasan Safety Riding SMKN4 Tasikmalaya</h5>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
@@ -40,7 +40,7 @@
       </div>
       <div class="col">
         <div class="card">
-          <img src="/img/srl.jpg" class="card-img-top" alt="Safety Riding">
+          <img src="/img/galery5.jpeg" class="card-img-top" alt="Safety Riding">
           <div class="card-body">
             <h5 class="card-title">Pengujian Lintasan Safety Riding SMKN4 Tasikmalaya</h5>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
@@ -49,13 +49,149 @@
       </div>
       <div class="col">
         <div class="card">
-          <img src="/img/prestasi.jpg" class="card-img-top" alt="Prestasi">
+          <img src="/img/galery6.jpeg" class="card-img-top" alt="Prestasi">
           <div class="card-body">
             <h5 class="card-title">Prestasi Lainnya</h5>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
           </div>
         </div>
       </div>
+      <div class="col">
+        <div class="card">
+          <img src="/img/galery7.jpeg" class="card-img-top" alt="Safety Riding">
+          <div class="card-body">
+            <h5 class="card-title">Pengujian Lintasan Safety Riding SMKN4 Tasikmalaya</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="/img/galery8.jpeg" class="card-img-top" alt="Safety Riding">
+          <div class="card-body">
+            <h5 class="card-title">Pengujian Lintasan Safety Riding SMKN4 Tasikmalaya</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="/img/galery9.jpeg" class="card-img-top" alt="Prestasi">
+          <div class="card-body">
+            <h5 class="card-title">Prestasi Lainnya</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="/img/galery10.jpeg" class="card-img-top" alt="Safety Riding">
+          <div class="card-body">
+            <h5 class="card-title">Pengujian Lintasan Safety Riding SMKN4 Tasikmalaya</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="/img/galery11.jpeg" class="card-img-top" alt="Safety Riding">
+          <div class="card-body">
+            <h5 class="card-title">Pengujian Lintasan Safety Riding SMKN4 Tasikmalaya</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="/img/galery12.jpeg" class="card-img-top" alt="Prestasi">
+          <div class="card-body">
+            <h5 class="card-title">Prestasi Lainnya</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="/img/galery13.jpeg" class="card-img-top" alt="Safety Riding">
+          <div class="card-body">
+            <h5 class="card-title">Pengujian Lintasan Safety Riding SMKN4 Tasikmalaya</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="/img/galery14.jpeg" class="card-img-top" alt="Safety Riding">
+          <div class="card-body">
+            <h5 class="card-title">Pengujian Lintasan Safety Riding SMKN4 Tasikmalaya</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="/img/galery15.jpeg" class="card-img-top" alt="Prestasi">
+          <div class="card-body">
+            <h5 class="card-title">Prestasi Lainnya</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="/img/galery16.jpeg" class="card-img-top" alt="Safety Riding">
+          <div class="card-body">
+            <h5 class="card-title">Pengujian Lintasan Safety Riding SMKN4 Tasikmalaya</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="/img/galery17.jpeg" class="card-img-top" alt="Safety Riding">
+          <div class="card-body">
+            <h5 class="card-title">Pengujian Lintasan Safety Riding SMKN4 Tasikmalaya</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="/img/galery18.jpeg" class="card-img-top" alt="Prestasi">
+          <div class="card-body">
+            <h5 class="card-title">Prestasi Lainnya</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="/img/galery19.jpeg" class="card-img-top" alt="Safety Riding">
+          <div class="card-body">
+            <h5 class="card-title">Pengujian Lintasan Safety Riding SMKN4 Tasikmalaya</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="/img/galery20.jpeg" class="card-img-top" alt="Safety Riding">
+          <div class="card-body">
+            <h5 class="card-title">Pengujian Lintasan Safety Riding SMKN4 Tasikmalaya</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="/img/galery21.jpeg" class="card-img-top" alt="Prestasi">
+          <div class="card-body">
+            <h5 class="card-title">Prestasi Lainnya</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+      </div>
+
       <!-- Add more cards as needed, truncated for brevity -->
     </div>
   </div>
